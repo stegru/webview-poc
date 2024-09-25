f4a06e559c78bc3c5908ce370105b72e9ff37257
Steve Grundell <git@grundell.co.uk>
2024-09-25 15:53:40 +0100

    Initial commit

---
+++ winui-test-app/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml.Controls;
+using WinRT.Interop;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winui_test_app;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+
+    }
+}
